8c33d839ad07828a35d3ca6cc93518006102673e
Manav Dewangan <manavdewangan2017@gmail.com>
2025-10-16 22:41:20 +0530

    update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/leaderboard.html', (req, res) => {
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
 
 
